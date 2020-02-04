0cf638d02690dfe78c79a9dbe8a5bfe20a6c826f
01Skymon10 <nour.khedher@esprit.tn>
2020-02-05 00:35:34 +0100

    Symfony

---
+++ Baskel/src/BaskelBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BaskelBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction()
+    {
+        return $this->render('@Baskel/Default/index.html.twig');
+    }
+}
