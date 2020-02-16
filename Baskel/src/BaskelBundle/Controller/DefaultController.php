2d916648d3cf673ec8ea795cf471ec5bbc1267c0
01Skymon10 <nour.khedher@esprit.tn>
2020-02-16 19:40:21 +0100

    pdf

--- Baskel/src/BaskelBundle/Controller/DefaultController.php
+++ Baskel/src/BaskelBundle/Controller/DefaultController.php
@@ -3,6 +3,7 @@
 namespace BaskelBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
@@ -13,5 +14,21 @@ class DefaultController extends Controller
     public function indexAdminAction()
     {
         return $this->render('@Baskel/Default/indexadmin.html.twig');
+    }
+
+    public function pdfAction(){
+        $snappy = $this->get('knp_snappy.pdf');
+        $filename = 'myFirstSnappyPDF';
+        $url = 'http://ourcodeworld.com';
+
+
+        return new Response(
+            $snappy->getOutput($url),
+            200,
+            array(
+                'Content-Type'          => 'application/pdf',
+                'Content-Disposition'   => 'inline; filename="'.$filename.'.pdf"'
+            )
+        );
     }
 }
