45b84864eca141172bd2c46b714e68e90b528c14
achraf9585 <achraf9585@gmail.com>
2020-02-22 01:58:41 +0100

    V BETA
    fama changes quand méme

--- Baskel/src/BaskelBundle/Controller/LivreurController.php
+++ Baskel/src/BaskelBundle/Controller/LivreurController.php
@@ -27,6 +27,7 @@ class LivreurController extends  Controller
     }
 
     public  function  modifEtatAction($id,Request $request){
+
         $em= $this->getDoctrine()->getManager();
         $livreur=$em->getRepository(User::class)->find($id);
         $form=$this->createForm(modifEtatType::class,$livreur);
@@ -35,6 +36,26 @@ class LivreurController extends  Controller
         {
             $em->persist($livreur);
             $em->flush();
+            //MAIL
+            if($livreur->getEtat()=="accepté") {
+
+
+                $message = (new \Swift_Message('Hello Email'))
+                    ->setFrom('achraf9585@gmail.com')
+                    ->setTo($livreur->getEmail())
+                    ->setBody(
+                        $this->renderView(
+                        // templates/emails/registration.html.twig
+                            'BaskelBundle:Livreur:mail.txt.twig'
+
+                ),
+                        'text/html'
+                    )// you can remove the following code if you don't define a text version for your emails
+
+                ;
+
+                $this->get('mailer')->send($message);            }
+
             return $this->redirectToRoute('consliv');
         }
         return $this->render('@Baskel/Livreur/modifetat.html.twig',array('form'=>$form->createView(),'livreur'=>$livreur));
@@ -53,7 +74,7 @@ class LivreurController extends  Controller
         }
         return $this->render('@Baskel/Livreur/modifisolde.html.twig',array('form'=>$form->createView(),'livreur'=>$livreur));
     }
-    public function pdfAction($id)
+    public function pdfAction($id,Request $request)
     {
         $em=$this->getDoctrine()->getManager();
         $user=$em->getRepository(User::class)->find($id);
@@ -62,6 +83,7 @@ class LivreurController extends  Controller
         $html = $this -> renderView("@Baskel/Livreur/pdf.html.twig", array(
                 'user'=>$user,
                 'vehicule'=>$vehicule,
+                'rootDir' => $this->get('kernel')->getRootDir().'/..'
             )
         );
         $filename="fichel";
